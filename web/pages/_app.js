25148e5d86098ffcfd5a16536dbb4cfcd0909cc3
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-10-30 22:22:08 -0500

    [ADD] read query

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,3 +1,4 @@
+import { ReactQueryDevtools } from "react-query-devtools";
 import { Container } from "@glrodasz/components";
 
 import "minireset.css";
@@ -8,6 +9,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <Container>
       <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen />
     </Container>
   );
 }
