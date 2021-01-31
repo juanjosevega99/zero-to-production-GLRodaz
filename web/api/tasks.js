11c6a8b69b549b12337cf2b713a2dc83bc328dd7
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2021-01-31 15:47:19 -0500

    feat(planning): implement drag and drop

--- web/api/tasks.js
+++ web/api/tasks.js
@@ -2,16 +2,28 @@ import Request from './request'
 
 class Task extends Request {
   getAll() {
-    return this.request()
+    return this.request('tasks?_sort=priority&_order=asc')
   }
 
   getById({ id }) {
     return this.request(`tasks/${id}`)
   }
 
-  create({ description }) {
-    return this.request(`tasks`, { method: 'post', body: { description } })
+  create({ description, priority }) {
+    return this.request('tasks', {
+      method: 'post',
+      body: { description, priority },
+    })
   }
+
+  updatePriorities({ tasks }) {
+    return Promise.all(
+      tasks.map(({ id, priority }) =>
+        this.request(`tasks/${id}`, { method: 'patch', body: { priority } })
+      )
+    )
+  }
+
   delete({ id }) {
     return this.request(`tasks/${id}`, { method: 'delete' })
   }
